18f808832b2fb21656a07565f0b15fac95589cd3
witoldmetel <witoldmetel@gmail.com>
2023-10-02 14:00:10 +0200

    Adjust styles for kanban board

--- src/core/layouts/Container/Container.tsx
+++ src/core/layouts/Container/Container.tsx
@@ -5,28 +5,54 @@ import './Container.scss';
 export const Container = () => {
   return (
     <div className="container">
-      <div className="container__board">
-        <h1>Board One</h1>
+      <div>
+        <div className="container__board">
+          <h1>Board One</h1>
 
-        <div className="container__board__card">Card #1</div>
-        <div className="container__board__card">Card #2</div>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+          <div className="container__board__card">
+            <p>Card #2</p>
+          </div>
+        </div>
       </div>
-      <div className="container__board">
-        <h1>Board Two</h1>
 
-        <div className="container__board__card">Card #1</div>
-        <div className="container__board__card">Card #2</div>
-        <div className="container__board__card">Card #3</div>
-        <div className="container__board__card">Card #4</div>
+      <div>
+        <div className="container__board">
+          <h1>Board Two</h1>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+        </div>
       </div>
-      <div className="container__board">
-        <h1>Board Three</h1>
 
-        <div className="container__board__card">Card #1</div>
-        <div className="container__board__card">Card #2</div>
-        <div className="container__board__card">Card #3</div>
+      <div>
+        <div className="container__board">
+          <h1>Board Three</h1>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+          <div className="container__board__card">
+            <p>Card #1</p>
+          </div>
+        </div>
       </div>
-      <ListCreator />
+      <div>
+        <ListCreator />
+      </div>
     </div>
   );
 };
